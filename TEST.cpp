df97653cd48eae8d1f9188709885b767c761e00e
avichauhan6832 <avi.r.chauhan6832@gmail.com>
2020-01-16 13:13:42 +0530

    first commit

---
+++ TEST.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long
+
+const string normalChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ";
+const int N=1e6+5;
+
+int32_t main()
+{
+	IOS;
+	/*freopen("apache.in","r",stdin);
+    freopen("apache.out","w",stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);*/
+    int x; cin >> x;
+    while(x != 42) {
+        cout<<x<<endl;
+        cin >> x;
+    }
+
+    return 0;
+}
